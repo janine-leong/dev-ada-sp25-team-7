5ff7cb9e7901b79796b170f1869eb02dd4d50f0a
Isabella <chou.isabella@gmail.com>
2025-03-09 15:51:50 -0500

    Added scripting, changed icon, added buttons
    Included scripting + permissions in manifest.json, changed the extension icon and added an image in popup, added buttons that show alerts.

--- popup.js
+++ popup.js
@@ -1,1 +1,25 @@
 console.log("This is a popup!")
+
+// followed this YouTube tutorial: https://www.youtube.com/watch?v=GGi7Brsf7js&ab_channel=TrainToCode
+async function petMe() {
+    let[tab] = await chrome.tabs.query({ active: true });
+    chrome.scripting.executeScript({
+        target: { tabId: tab.id },
+        func: () => {
+            alert("Yay! Thank you for petting me! 💚");
+        }
+    });
+}
+
+async function feedMe() {
+    let[tab] = await chrome.tabs.query({ active: true });
+    chrome.scripting.executeScript({
+        target: { tabId: tab.id },
+        func: () => {
+            alert("Yum! Thank you for feeding me! 😋");
+        }
+    });
+}
+
+document.getElementById("petButton").addEventListener("click", petMe);
+document.getElementById("feedButton").addEventListener("click", feedMe);
